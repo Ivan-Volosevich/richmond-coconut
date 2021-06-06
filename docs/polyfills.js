04f19edd9ae7290695102ca6bdc8ed339d8ce29a
ivanvolosevich <ivan.a.volosevich@mail.ru>
2021-06-06 19:39:26 +0300

    some fixes 06.06.21

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Волосевич\CodeFolders\richmond-coconut\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\Иван\Desktop\HTML, CSS = JS\coconut\richmond-coconut\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
