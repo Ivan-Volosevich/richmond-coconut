55846cbd07f15ea8edaba4c72a2158b894f5cee7
Ivan-Volosevich <rainland-96@mail.ru>
2021-06-10 15:57:41 +0300

    10.06 with fixes css

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Ivan Volosevich\richmond-coconut\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\Волосевич\CodeFolders\richmond-coconut\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
